4dd34111766ca6b81a7d696ea466e591c3a79fcb
Roger Allen <rallen@gmail.com>
2017-11-12 11:25:27 -0800

    Add names, fix issue.
    Adding names to each layer allowed me to see the final DB layer was
    not being added.  Fixed & getting better results.

--- train/tiramisu.py
+++ train/tiramisu.py
@@ -48,7 +48,7 @@ from keras.models import Model
 from keras.regularizers import l2
 import keras.backend as K
 
-def __dense_block_down(x, nb_layers, growth_rate, p, wd):
+def __dense_block_down(x, nb_layers, growth_rate, p, wd, name):
     """Described in Figure 1 & 2 from the paper.  A dense_block has
     nb_layers of 'Layer' blocks.
 
@@ -58,22 +58,22 @@ def __dense_block_down(x, nb_layers, growth_rate, p, wd):
     Note in Figure 1 where the down pathway has a post-DB concat of
     the x input and the up connection does not.  This 'down'
     dense_block returns a concatenation of x and the intermediate
-    layer outputs: [x,a,a',a'',a''',...].
+    layer outputs: [x,a,a',a'',...].
 
     """
-    with K.name_scope('DenseBlockDown'):
-        for i in range(nb_layers):
-            a = BatchNormalization(axis=-1)(x)
-            a = Activation('relu')(a)
-            a = Conv2D(growth_rate, (3, 3),
-                       padding="same",
-                       kernel_initializer="he_uniform",
-                       kernel_regularizer=l2(wd))(a)
-            a = Dropout(p)(a) if p else a
-            x = concatenate([x, a])
+    for i in range(nb_layers):
+        a = BatchNormalization(axis=-1,name=name+f'_{i}_batchnorm')(x)
+        a = Activation('relu',name=name+f'_{i}_relu')(a)
+        a = Conv2D(growth_rate, (3, 3),
+                   padding="same",
+                   kernel_initializer="he_uniform",
+                   kernel_regularizer=l2(wd),
+                   name=name+f'_{i}_conv2d')(a)
+        a = Dropout(p, name=name+f'_{i}_dropout')(a) if p else a
+        x = concatenate([x, a],name=name+f'_{i}_concat')
     return x
 
-def __dense_block_up(x, nb_layers, growth_rate, p, wd):
+def __dense_block_up(x, nb_layers, growth_rate, p, wd, name):
     """Described in Figure 1 & 2 from the paper.  A dense_block has
     nb_layers of 'Layer' blocks.
 
@@ -83,26 +83,26 @@ def __dense_block_up(x, nb_layers, growth_rate, p, wd):
     Note in Figure 1 where the down pathway has a post-DB concat of
     the x input and the up connection does not.  This 'up' dense_block
     returns a concatenated layer of only the added layer outputs
-    [a,a',a'',a''',...], not including the original input.
+    [a,a',a'',...], not including the original input.
 
     """
-    with K.name_scope('DenseBlockUp'):
-        for i in range(nb_layers):
-            a = BatchNormalization(axis=-1)(x)
-            a = Activation('relu')(a)
-            a = Conv2D(growth_rate, (3, 3),
-                       padding="same",
-                       kernel_initializer="he_uniform",
-                       kernel_regularizer=l2(wd))(a)
-            a = Dropout(p)(a) if p else a
-            x = concatenate([x, a])
-            if i == 0:
-                added = a
-            else:
-                added = concatenate([added,a])
+    for i in range(nb_layers):
+        a = BatchNormalization(axis=-1, name=name+f'_{i}_batchnorm')(x)
+        a = Activation('relu', name=name+f'_{i}_relu')(a)
+        a = Conv2D(growth_rate, (3, 3),
+                   padding="same",
+                   kernel_initializer="he_uniform",
+                   kernel_regularizer=l2(wd),
+                   name=name+f'_{i}_conv2d')(a)
+        a = Dropout(p, name=name+f'_{i}_dropout')(a) if p else a
+        x = concatenate([x, a], name=name+f'_{i}_concat_xa')
+        if i == 0:
+            added = a
+        else:
+            added = concatenate([added,a], name=name+f'_{i}_concat_aa')
     return added
 
-def __transition_down(x, p, wd, do_maxpool):
+def __transition_down(x, p, wd, do_maxpool, name):
     """See Table 1 for the Transition Down block.  Similar to the Layer in
     the DenseBlock, but slightly different with 1x1 Conv and 2x2
     MaxPooling.  Jeremy Howard's fast.ai code did not use MaxPooling,
@@ -110,32 +110,32 @@ def __transition_down(x, p, wd, do_maxpool):
     do_maxpool.
 
     """
-    with K.name_scope('TransitionDown'):
-        nb_filter = x.get_shape().as_list()[-1]
-        conv_stride = 1 if do_maxpool else 2
-        x = BatchNormalization(axis=-1)(x)
-        x = Activation('relu')(x)
-        x = Conv2D(nb_filter, (1, 1),
-                   padding="same",
-                   kernel_initializer="he_uniform",
-                   strides=(conv_stride, conv_stride),
-                   kernel_regularizer=l2(wd))(x)
-        x = Dropout(p)(x) if p else x
-        x = MaxPooling2D()(x) if do_maxpool else x
+    nb_filter = x.get_shape().as_list()[-1]
+    conv_stride = 1 if do_maxpool else 2
+    x = BatchNormalization(axis=-1, name=name+'_batchnorm')(x)
+    x = Activation('relu', name=name+'_relu')(x)
+    x = Conv2D(nb_filter, (1, 1),
+               padding="same",
+               kernel_initializer="he_uniform",
+               strides=(conv_stride, conv_stride),
+               kernel_regularizer=l2(wd),
+               name=name+'_conv2d')(x)
+    x = Dropout(p, name=name+'_dropout')(x) if p else x
+    x = MaxPooling2D(name=name+'_maxpool')(x) if do_maxpool else x
     return x
 
-def __transition_up(added, wd=0):
+def __transition_up(added, wd=0, name="TU"):
     """See Table 1 for the Transition Up block. A 3x3 Transposed Conv with
     stride=2.
 
     """
-    with K.name_scope('TransitionUp'):
-        _,r,c,ch = added.get_shape().as_list()
-        x = Conv2DTranspose(ch, (3, 3),
-                            strides=(2, 2),
-                            padding="same",
-                            kernel_initializer="he_uniform",
-                            kernel_regularizer=l2(wd))(added)
+    _,r,c,ch = added.get_shape().as_list()
+    x = Conv2DTranspose(ch, (3, 3),
+                        strides=(2, 2),
+                        padding="same",
+                        kernel_initializer="he_uniform",
+                        kernel_regularizer=l2(wd),
+                        name=name+'_conv2d')(added)
     return x
 
 def Tiramisu(nb_classes,
@@ -184,37 +184,42 @@ def Tiramisu(nb_classes,
 
     assert type(nb_layers_per_block) == list
 
-    image_input = Input(shape=input_shape)
+    image_input = Input(shape=input_shape,name="image_input")
 
     # initial 3x3 Convolution
     x = Conv2D(initial_filter, (3, 3), padding="same",
                kernel_initializer="he_uniform",
-               kernel_regularizer=l2(wd))(image_input)
+               kernel_regularizer=l2(wd),
+               name="initial_3x3_conv2d")(image_input)
 
     # Down Path: DenseBlocks + TransitionDowns
     skips = []
-    for nb_layers in nb_layers_per_block:
-        x = __dense_block_down(x, nb_layers, growth_rate, p, wd)
+    for i,nb_layers in enumerate(nb_layers_per_block):
+        #print(f"down {i} {nb_layers}")
+        x = __dense_block_down(x, nb_layers, growth_rate, p, wd, f'DB_DN{i}')
         skips.append(x)
-        x = __transition_down(x, p, wd, do_td_maxpool)
+        x = __transition_down(x, p, wd, do_td_maxpool, f'TD{i}')
 
     # Bottleneck
-    added = __dense_block_up(x, bottleneck_layers, growth_rate, p, wd)
+    added = __dense_block_up(x, bottleneck_layers, growth_rate, p, wd, 'DB_bottleneck')
 
     # Up Path: TransitionUp + DenseBlocks
     skips = list(reversed(skips))
     nb_layers_per_block = list(reversed(nb_layers_per_block))
     for i,nb_layers in enumerate(nb_layers_per_block):
-        x = __transition_up(added, wd)
-        x = concatenate([x,skips[i]])
-        added = __dense_block_up(x, nb_layers, growth_rate, p, wd)
+        ir = len(nb_layers_per_block)-1-i
+        #print(f"up {ir} {nb_layers}")
+        x = __transition_up(added, wd, f'TU{ir}')
+        x = concatenate([x,skips[i]], name=f"skip{ir}_concat")
+        added = __dense_block_up(x, nb_layers, growth_rate, p, wd, f'DB_UP{ir}')
 
     # final 1x1 Convolution & Softmax
     x = Conv2D(nb_classes, (1, 1), padding="same",
                kernel_initializer="he_uniform",
-               kernel_regularizer=l2(wd))(x)
-    x = Reshape((-1, nb_classes))(x)
-    x = Activation('softmax')(x)
+               kernel_regularizer=l2(wd),
+               name="final_conv2d")(added)
+    x = Reshape((-1, nb_classes), name="final_reshape")(x)
+    x = Activation('softmax', name="final_softmax")(x)
 
     model = Model(image_input, x, name='tiramisu')
 
