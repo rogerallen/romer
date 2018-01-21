f645f80d61863f8ca5a823b0f82f3130ffd069d6
Roger Allen <rallen@gmail.com>
2018-01-20 16:06:55 -0800

    create more, better tests.
    found that only chromatic created low A which explains how that
    could've been missed.

--- setup/src/gen_test_scores.py
+++ setup/src/gen_test_scores.py
@@ -7,9 +7,7 @@ ly_template_start = """\\include "event-romer.ly"
 }
 \\score {
   {
-    \\clef treble
-    \\numericTimeSignature
-    \\time 4/4"""
+    \\clef treble"""
 
 ly_template_end = """  }
   \\layout { }
@@ -20,19 +18,18 @@ ly_template_end = """  }
 def gen_test_one():
     name = "gen_one.ly"
     print("generating: %s"%(name))
-    pitches = "c cis des d dis ees e f fis ges g gis aes a ais bes b r".split()
-    octaves = [ "'", "''" ]
+    pitches = """\
+aes a ais bes b c' cis' des' d' dis' ees' e' f' fis' ges' g' gis' r
+aes' a' ais' bes' b' c'' cis'' des'' d'' dis'' ees'' e'' f'' fis'' ges'' g'' gis'' r
+aes'' a'' ais'' bes'' b'' c''' cis''' r""".split()
     lengths = [ "1", "2", "4", "8", "16" ]
     with open(name,'w') as f:
         print(ly_template_start,file=f)
+        print("    \\numericTimeSignature\n    \\time 4/4",file=f)
         count = 0
         for l in lengths:
-            for o in octaves:
                 for p in pitches:
-                    if p != 'r':
-                        print("%s%s%s"%(p,o,l),end=' ',file=f)
-                    else:
-                        print("%s%s"%(p,l),end=' ',file=f)
+                    print("%s%s"%(p,l),end=' ',file=f)
                     count += 1
                     if count%8==0:
                         print(file=f)
@@ -45,9 +42,9 @@ def gen_test_two():
     pitches = "c cis des d dis ees e f fis ges g gis aes a ais bes b r".split()
     octaves = [ "'", "''" ]
     lengths = ["4"]*4 + ["8"]*4 + ["2", "1"]
-    [ "1", "2", "4", "8" ]
     with open(name,'w') as f:
         print(ly_template_start,file=f)
+        print("    \\numericTimeSignature\n    \\time 4/4",file=f)
         count = 0
         li = 0
         for i in range(8):
@@ -64,9 +61,53 @@ def gen_test_two():
                     li = (li+1)%len(lengths)
         print(ly_template_end,file=f)
 
+def gen_test_three(index):
+    seed(index*1234)  # random, but deterministic behavior
+    name = f"gen_three_{index}.ly"
+    print("generating: %s"%(name))
+    pitches = "c cis des d dis ees e f fis ges g gis aes a ais bes b r r r".split()
+    octaves = [ "", "'", "''", "'''" ]
+    lengths = [ ["1"], # need to create even measures
+                ["2", "4", "4"],
+                ["4", "4", "4", "8", "8", ],
+                ["4", "4", "8", "8", "8", "8", ],
+                ["4", "8", "8", "8", "8", "8", "8", ],
+                ["8", "8", "8", "8", "8", "8", "8", "8", ],
+                ["4", "4", "8", "8", "16", "16", "16", "16"]]
+    with open(name,'w') as f:
+        print(ly_template_start,file=f)
+        count = 0
+        for i in range(64):
+            shuffle(pitches)
+            length = choice(lengths)
+            shuffle(length)
+            o = choice(octaves)
+            li = 0
+            for p in pitches:
+                if p != 'r':
+                    if o == "":
+                        if p not in "aes a ais bes b".split():
+                            o = "'"
+                    if o == "'''":
+                        if p not in "ces c cis".split():
+                            o = "''"
+                    print("%s%s%s"%(p,o,length[li]),end=' ',file=f)
+                else:
+                    print("%s%s"%(p,length[li]),end=' ',file=f)
+                count += 1
+                if count%8==0:
+                    print(file=f)
+                li += 1
+                if li == len(length):
+                    break
+        print(ly_template_end,file=f)
+
 def main():
     gen_test_one()
     gen_test_two()
+    gen_test_three(1)
+    gen_test_three(2)
+    gen_test_three(3)
 
 if __name__ == "__main__":
     main()
